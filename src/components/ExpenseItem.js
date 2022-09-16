e1bc6fe6dd8794e415ecd609920d3d6fc132b979
Karan <karanchandekar431@gmail.com>
2022-09-16 21:53:42 +0530

    added basic jsx code

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>September 16th 2022</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$246.75</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
