5465cf37904a38699904f838ec0fb41ddbba9888
Karan <karanchandekar431@gmail.com>
2023-01-03 17:55:32 +0530

    fixed a small bug

--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -11,10 +11,10 @@ const ExpensesFilter = (props) => {
       <div className="expenses-filter__control">
         <label>Filter by year</label>
         <select value={props.selected} onChange={dropdownChangeHandler}>
-          <option value="2022">2022</option>
-          <option value="2021">2021</option>
-          <option value="2020">2020</option>
-          <option value="2019">2019</option>
+          <option value="2022">2023</option>
+          <option value="2021">2022</option>
+          <option value="2020">2021</option>
+          <option value="2019">2020</option>
         </select>
       </div>
     </div>
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -50,7 +50,7 @@ const ExpenseForm = (props) => {
 
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
 
