4b1d687b162a818a48fd49bc08e85f89b6c5f16b
Karan <karanchandekar431@gmail.com>
2023-01-03 14:45:59 +0530

    wrap up

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -4,7 +4,7 @@ import "./ChartBar.css";
 const ChartBar = (props) => {
   let barFillHeight = "0%";
 
-  if (props.max > 0) {
+  if (props.maxValue > 0) {
     barFillHeight = Math.round((props.value / props.maxValue) * 100) + "%";
   }
   return (
@@ -14,8 +14,8 @@ const ChartBar = (props) => {
           className="chart-bar__fill"
           style={{ height: barFillHeight }}
         ></div>
-        <div className="chart-bar__label">{props.label}</div>
       </div>
+      <div className="chart-bar__label">{props.label}</div>
     </div>
   );
 };
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,6 +4,7 @@ import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -23,7 +24,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
