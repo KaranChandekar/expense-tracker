c8ed5a85bf3d88fc904032ee1db8599fd6a10bd6
Karan <karanchandekar431@gmail.com>
2023-01-03 12:03:31 +0530

    understanding and adding keys

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -22,6 +22,7 @@ const Expenses = (props) => {
 
         {props.items.map((expense) => (
           <ExpenseItem
+            key={expense.id}
             title={expense.title}
             amount={expense.amount}
             date={expense.date}
